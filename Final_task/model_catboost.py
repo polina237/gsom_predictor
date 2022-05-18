f6abb0cd5d3f9dcd5dc99a76a153906bab65dc4c
polina_student <st061467@gsom.spbu.ru>
2022-05-18 14:47:21 +0300

    Model with forest and catboost

--- Final_task/model_catboost.py
+++ Final_task/model_catboost.py
@@ -2,19 +2,20 @@ from flask import Flask, request
 
 app=Flask(__name__)
 
-MODEL_PATH="for_models/model_forest.pkl"
-SCALER_X_PATH="for_models/scaler_x_forest.pkl"
-SCALER_Y_PATH="for_models/scaler_y_forest.pkl"
+MODEL_PATH="for_models/model_catboost.pkl"
+SCALER_X_PATH="for_models/scaler_x_catboost.pkl"
+SCALER_Y_PATH="for_models/scaler_y_catboost.pkl"
+
 
 @app.route("/predict_price", methods=['GET'])
 def predict():
     args=request.args
-    amount = args.get('amount', default=-1, type=int)
     rooms=args.get('rooms', default=-1, type=int)
     area=args.get('area', default=-1, type=float)
     renovation=args.get('renovation', default=-1,type=int)
-    response="amount:{},rooms:{}, area:{}, renovation:{}".format(amount,rooms, area, renovation)
+    response="rooms:{}, area:{}, renovation:{}".format(rooms, area, renovation)
     return response
+
 
 if __name__=='__main__':
     app.run(debug=True, port=5444, host='0.0.0.0')
